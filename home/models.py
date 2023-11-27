f58a491cee436de30d50f2056a3b3b4f00430334
= <joacontre0@gmail.com>
2023-11-27 00:04:53 -0300

    Problems with user model

--- home/models.py
+++ home/models.py
@@ -1,20 +1,71 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager,PermissionsMixin
 
 
-# Create your models here.
-class User(AbstractUser):   
+class UsuarioManager(BaseUserManager):
+    def create_user(self, email, username, first_name, last_name, dni, password=None):
+        if not email:
+            raise ValueError("The email is required")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            dni=dni,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, first_name, last_name, dni, password):
+        user = self.create_user(
+            email,
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            dni=dni,
+            password=password
+        )
+        user.admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractUser, PermissionsMixin):
+    username = models.CharField('username', max_length=150, unique=True)
+    email = models.EmailField('email address', unique=True, max_length = 255)
     biography = models.TextField()
-    images = models.ImageField(upload_to='user/%Y/%m/%d', null=True, blank=True)
+    images = models.ImageField(upload_to="user/%Y/%m/%d", null=True, blank=True)
     dni = models.CharField(max_length=20, null=False, blank=False)
+    admin = models.BooleanField(default=False)
+    first_name = models.CharField('first_name', max_length=150, unique=False)
+    last_name = models.CharField('last_name', max_length=150, unique=False)
+    dni = models.CharField('last_name', max_length=150, unique=True)
+    objects = UsuarioManager()
+    
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self) -> str:
+        return f"{self.first_name},{self.last_name}"
+
+    @property
+    def is_staff(self):
+        return self.admin
+
     # telefono
+
 
 class comments(models.Model):
     comment_id = models.BigAutoField(primary_key=True)
-    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
     approved_comment = models.BooleanField(default=False)
+
 
 class rents(models.Model):
     rent_id = models.BigAutoField(primary_key=True)
@@ -28,7 +79,7 @@ class rents(models.Model):
     reviews_nb = models.DecimalField(decimal_places=10, max_digits=20)
     reviews_avg = models.DecimalField(decimal_places=5, max_digits=20)
     comments = models.ForeignKey(comments, on_delete=models.CASCADE)
-    bedrooms = models.DecimalField(decimal_places=4 ,max_digits=15)
-    bathrooms = models.DecimalField(decimal_places=4 ,max_digits=15)
-    total_guests_limit = models.DecimalField(decimal_places=4 ,max_digits=15)
-    images = models.ImageField(upload_to='rent_images/%Y/%m/%d', null=True, blank=True)
+    bedrooms = models.DecimalField(decimal_places=4, max_digits=15)
+    bathrooms = models.DecimalField(decimal_places=4, max_digits=15)
+    total_guests_limit = models.DecimalField(decimal_places=4, max_digits=15)
+    images = models.ImageField(upload_to="rent_images/%Y/%m/%d", null=True, blank=True)
--- home/views.py
+++ home/views.py
@@ -1,46 +1,48 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth import get_user_model
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from home.forms import CustomUserCreationForm
 
-# Create your views here.
+
+# get_user_model is use to get the personalize user_model
+User = get_user_model()
+
+
 # @login_required
 def root(request):
-    return render(request, "index.html")
+    return render(request, "home.html")
+
 
 def signup(request):
-
     if request.method == "GET":
-        return render(request, "signup.html", {
-            'form': UserCreationForm
-        })
+        return render(request, "signup.html", {"form": CustomUserCreationForm()})
     else:
-        if request.POST['password1'] == request.POST['password2']:
-            # try:
-                print(request.POST)
-                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'], dni=request.POST['dni'])
-
-                print(user)
-
+        if request.POST["password1"] == request.POST["password2"]:
+            # user_creation_form = CustomUserCreationForm(data=request.POST)
+            # if user_creation_form.is_valid():
+            #     user_creation_form.save()
+            try:
+                user = User.objects.create_user(
+                    username=request.POST["username"],
+                    password=request.POST["password1"],
+                    dni=request.POST["dni"],
+                )
                 user.save()
+                # user = authenticate(username=user_creation_form.cleaned_data['username'], password=user_creation_form.cleaned_data['password1'])
                 login(request, user)
                 return redirect("home")
-            # except:
-            #     return render(request, "signup.html", {
-            #         'form': UserCreationForm,
-            #         'error': "User already exists"
-            #         })
+            except:
+                return render(
+                    request,
+                    "signup.html",
+                    {"form": CustomUserCreationForm(), "error": "User already exists"},
+                )
 
-        return render(request, "signup.html", {
-            'form': UserCreationForm,
-            'error': "Passwords don't match"
-            })
-
-
-
-
-
-
+        return render(
+            request,
+            "signup.html",
+            {"form": CustomUserCreationForm(), "error": "Passwords don't match"},
+        )
